bc117765aacd6142ba43ec82d106724647929973
Andrew Connell <me@andrewconnell.com>
2019-11-15 06:41:55 -0500

    Initial commit of module resources

---
+++ demos/01-reading-users/Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security;
+using Microsoft.Identity.Client;
+using Microsoft.Graph;
+using Microsoft.Extensions.Configuration;
+using Helpers;
+
+namespace graphusers01
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+
+      var config = LoadAppSettings();
+      if (config == null)
+      {
+        Console.WriteLine("Invalid appsettings.json file.");
+        return;
+      }
+
+      var userName = ReadUsername();
+      var userPassword = ReadPassword();
+
+      var client = GetAuthenticatedGraphClient(config, userName, userPassword);
+
+      // request 1 - all users
+      // var requestAllUsers = client.Users.Request();
+
+      // var results = requestAllUsers.GetAsync().Result;
+      // foreach (var user in results)
+      // {
+      //   Console.WriteLine(user.Id + ": " + user.DisplayName + " <" + user.Mail + ">");
+      // }
+
+      // Console.WriteLine("\nGraph Request:");
+      // Console.WriteLine(requestAllUsers.GetHttpRequestMessage().RequestUri);
+
+      // request 2 - current user
+      // var requestMeUser = client.Me.Request();
+
+      // var resultMe = requestMeUser.GetAsync().Result;
+      // Console.WriteLine(resultMe.Id + ": " + resultMe.DisplayName + " <" + resultMe.Mail + ">");
+
+      // Console.WriteLine("\nGraph Request:");
+      // Console.WriteLine(requestMeUser.GetHttpRequestMessage().RequestUri);
+
+      // request 3 - specific user
+      // var requestSpecificUser = client.Users.Request()
+      //                                       .Filter("Id eq '460c06cb-fe2c-4271-a65e-c42da1f724af'");
+      // var resultOtherUser = requestSpecificUser.GetAsync().Result[0];
+      var requestSpecificUser = client.Users["460c06cb-fe2c-4271-a65e-c42da1f724af"].Request();
+      var resultOtherUser = requestSpecificUser.GetAsync().Result;
+      Console.WriteLine(resultOtherUser.Id + ": " + resultOtherUser.DisplayName + " <" + resultOtherUser.Mail + ">");
+
+      Console.WriteLine("\nGraph Request:");
+      Console.WriteLine(requestSpecificUser.GetHttpRequestMessage().RequestUri);
+    }
+
+    private static IAuthenticationProvider CreateAuthorizationProvider(IConfigurationRoot config, string userName, SecureString userPassword)
+    {
+      var clientId = config["applicationId"];
+      var authority = $"https://login.microsoftonline.com/{config["tenantId"]}/v2.0";
+
+      List<string> scopes = new List<string>();
+      scopes.Add("User.Read");
+      scopes.Add("User.Read.All");
+
+      var cca = PublicClientApplicationBuilder.Create(clientId)
+                                              .WithAuthority(authority)
+                                              .Build();
+      return MsalAuthenticationProvider.GetInstance(cca, scopes.ToArray(), userName, userPassword);
+    }
+
+    private static GraphServiceClient GetAuthenticatedGraphClient(IConfigurationRoot config, string userName, SecureString userPassword)
+    {
+      var authenticationProvider = CreateAuthorizationProvider(config, userName, userPassword);
+      var graphClient = new GraphServiceClient(authenticationProvider);
+      return graphClient;
+    }
+
+    private static string ReadUsername()
+    {
+      string username;
+      Console.WriteLine("Enter your username");
+      username = Console.ReadLine();
+      return username;
+    }
+
+    private static SecureString ReadPassword()
+    {
+      Console.WriteLine("Enter your password");
+      SecureString password = new SecureString();
+      while (true)
+      {
+        ConsoleKeyInfo c = Console.ReadKey(true);
+        if (c.Key == ConsoleKey.Enter)
+        {
+          break;
+        }
+        password.AppendChar(c.KeyChar);
+        Console.Write("*");
+      }
+      Console.WriteLine();
+      return password;
+    }
+
+    private static IConfigurationRoot LoadAppSettings()
+    {
+      try
+      {
+        var config = new ConfigurationBuilder()
+                          .SetBasePath(System.IO.Directory.GetCurrentDirectory())
+                          .AddJsonFile("appsettings.json", false, true)
+                          .Build();
+
+        if (string.IsNullOrEmpty(config["applicationId"]) ||
+            string.IsNullOrEmpty(config["tenantId"]))
+        {
+          return null;
+        }
+
+        return config;
+      }
+      catch (System.IO.FileNotFoundException)
+      {
+        return null;
+      }
+    }
+  }
+}
---
+++ demos/03-modify-users/Program.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security;
+using Microsoft.Identity.Client;
+using Microsoft.Graph;
+using Microsoft.Extensions.Configuration;
+using Helpers;
+using System.Threading.Tasks;
+
+namespace graphusers01
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+
+      var config = LoadAppSettings();
+      if (config == null)
+      {
+        Console.WriteLine("Invalid appsettings.json file.");
+        return;
+      }
+
+      var userName = ReadUsername();
+      var userPassword = ReadPassword();
+
+      var client = GetAuthenticatedGraphClient(config, userName, userPassword);
+
+      // request 1: create user
+      // var resultNewUser = CreateUserAsync(client);
+      // resultNewUser.Wait();
+      // Console.WriteLine("New user ID: " + resultNewUser.Id);
+
+      // request 2: update user
+      // (1/2) get the user we just created
+      var userToUpdate = client.Users
+                                      .Request()
+                                      .Select("id")
+                                      .Filter("UserPrincipalName eq 'melissad@M365x068225.onmicrosoft.com'")
+                                      .GetAsync()
+                                      .Result[0];
+      // (2/2) update the user's phone number
+      var resultUpdatedUser = UpdateUserAsync(client, userToUpdate.Id);
+      resultUpdatedUser.Wait();
+      Console.WriteLine("Updated user ID: " + resultUpdatedUser.Id);
+
+      // request 3: delete user
+      var deleteTask = DeleteUserAsync(client, userToUpdate.Id);
+      deleteTask.Wait();
+    }
+
+    private static async Task<Microsoft.Graph.User> CreateUserAsync(GraphServiceClient client) {
+      Microsoft.Graph.User user = new Microsoft.Graph.User() {
+        AccountEnabled = true,
+        GivenName = "Melissa",
+        Surname = "Darrow",
+        DisplayName = "Melissa Darrow",
+        MailNickname = "MelissaD",
+        UserPrincipalName = "melissad@M365x068225.onmicrosoft.com",
+        PasswordProfile = new PasswordProfile() {
+          Password = "Password1!",
+          ForceChangePasswordNextSignIn = true
+        }
+      };
+      var requestNewUser = client.Users.Request();
+      return await requestNewUser.AddAsync(user);
+    }
+
+    private static async Task<Microsoft.Graph.User> UpdateUserAsync(GraphServiceClient client, string userIdToUpdate) {
+      Microsoft.Graph.User user = new Microsoft.Graph.User() {
+        MobilePhone = "555-555-1212"
+      };
+      return await client.Users[userIdToUpdate].Request().UpdateAsync(user);
+    }
+
+    private static async Task DeleteUserAsync(GraphServiceClient client, string userIdToDelete) {
+      await client.Users[userIdToDelete].Request().DeleteAsync();
+    }
+
+    private static IAuthenticationProvider CreateAuthorizationProvider(IConfigurationRoot config, string userName, SecureString userPassword)
+    {
+      var clientId = config["applicationId"];
+      var authority = $"https://login.microsoftonline.com/{config["tenantId"]}/v2.0";
+
+      List<string> scopes = new List<string>();
+      scopes.Add("User.Read");
+      scopes.Add("User.Read.All");
+      scopes.Add("User.ReadWrite.All");
+
+      var cca = PublicClientApplicationBuilder.Create(clientId)
+                                              .WithAuthority(authority)
+                                              .Build();
+      return MsalAuthenticationProvider.GetInstance(cca, scopes.ToArray(), userName, userPassword);
+    }
+
+    private static GraphServiceClient GetAuthenticatedGraphClient(IConfigurationRoot config, string userName, SecureString userPassword)
+    {
+      var authenticationProvider = CreateAuthorizationProvider(config, userName, userPassword);
+      var graphClient = new GraphServiceClient(authenticationProvider);
+      return graphClient;
+    }
+
+    private static string ReadUsername()
+    {
+      string username;
+      Console.WriteLine("Enter your username");
+      username = Console.ReadLine();
+      return username;
+    }
+
+    private static SecureString ReadPassword()
+    {
+      Console.WriteLine("Enter your password");
+      SecureString password = new SecureString();
+      while (true)
+      {
+        ConsoleKeyInfo c = Console.ReadKey(true);
+        if (c.Key == ConsoleKey.Enter)
+        {
+          break;
+        }
+        password.AppendChar(c.KeyChar);
+        Console.Write("*");
+      }
+      Console.WriteLine();
+      return password;
+    }
+
+    private static IConfigurationRoot LoadAppSettings()
+    {
+      try
+      {
+        var config = new ConfigurationBuilder()
+                          .SetBasePath(System.IO.Directory.GetCurrentDirectory())
+                          .AddJsonFile("appsettings.json", false, true)
+                          .Build();
+
+        if (string.IsNullOrEmpty(config["applicationId"]) ||
+            string.IsNullOrEmpty(config["tenantId"]))
+        {
+          return null;
+        }
+
+        return config;
+      }
+      catch (System.IO.FileNotFoundException)
+      {
+        return null;
+      }
+    }
+  }
+}
